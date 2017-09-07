d4cf0731c555d7d11538a37e074a8714cdc031de
NeilyoLee <blackkey520@126.com>
2017-09-07 14:14:41 +0800

    1.去除平台中dateformat的引用，删除dateformat插件 2.优化代码结构，部分跳转逻辑放到effact中实现，解决android平台React Navigation Title 首次跳覆盖的问题 3.集成 Jpush（android平台）

--- src/models/app.js
+++ src/models/app.js
@@ -1,8 +1,9 @@
 import { createAction, NavigationActions,ShowToast} from '../utils'
 import * as authService from '../services/authService'
-
-
-
+import * as AlarmService from '../services/alarmService'
+import { loadStorage} from '../logics/rpc';
+import JPushModule from 'jpush-react-native';
+import moment from 'moment'
 export default {
   namespace: 'app',
   state: {
@@ -28,11 +29,16 @@ export default {
         yield put(createAction('loginStart')({}));
         result = yield call(authService.getcontactlist,{})
         yield put(createAction('loginEnd')({ contactlist:result!=null?result:[]}))
+        yield put(
+          NavigationActions.navigate({
+            routeName: 'ContactList'
+          })
+        )
       },
       *loaduser({ payload: { user } }, { call, put }) {
         yield put(createAction('changeState')({ user:user }))
       },
-      *login({ payload: { username,password} }, { call, put }) {
+      *login({ payload: { username,password,pollutanttype} }, { call, put }) {
         let result=null;
         let ismaintenance=false;
         if(username==''|| password=='')
@@ -42,10 +48,20 @@ export default {
           yield put(createAction('loginStart')())
           result = yield call(authService.login, {username,password})
           if (result.message=='') {
+            JPushModule.setAlias(result.User_Account, (map) => {
+      				if (map.errorCode === 0) {
+      					console.log("set alias succeed");
+      				} else {
+      					console.log("set alias failed, errorCode: " + map.errorCode);
+      				}
+      			});
+            let alarmCount = yield call(AlarmService.loadawaitcheck,
+               {time:moment().format('YYYY-MM-DD')})
+             yield put({type: 'point/fetchmore',payload: {pollutantType:pollutanttype[0].ID}})
             yield put(
               NavigationActions.reset({
                 index: 0,
-                actions: [NavigationActions.navigate({ routeName: 'Main' })],
+                actions: [NavigationActions.navigate({ routeName: 'Main' ,params:{unverifiedCount:alarmCount.data.length,pollutanttype:pollutanttype} })],
               })
             )
           }else{
